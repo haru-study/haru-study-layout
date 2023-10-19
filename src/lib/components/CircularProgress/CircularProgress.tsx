77af4ba59424682fa4477c00ce7d8988f9fe035c
nlom0218 <nlom0218@naver.com>
2023-10-19 13:38:46 +0900

    feat: Button, CircularProgress 컴포넌트 생성

---
+++ src/lib/components/CircularProgress/CircularProgress.tsx
@@ -0,0 +1,90 @@
+import { css, styled } from 'styled-components';
+import type { CSSProp } from 'styled-components';
+
+import color from '../../styles/color';
+
+const SIZE = {
+  'x-small': '1.6rem',
+  small: '2.0rem',
+  medium: '2.4rem',
+  large: '2.8rem',
+  'x-large': '3.2rem',
+} as const;
+
+type Size = keyof typeof SIZE;
+
+type Props = {
+  size?: Size;
+  circleColor?: string;
+  $style?: CSSProp;
+};
+
+const CircularProgress = ({
+  size = 'medium',
+  circleColor = color.white,
+  $style,
+}: Props) => {
+  return (
+    <StyledCircularProgress
+      size={size}
+      $style={$style}
+      $circleColor={circleColor}
+    >
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </StyledCircularProgress>
+  );
+};
+
+export default CircularProgress;
+
+type StyledCircularProgressProps = {
+  size?: Size;
+  $circleColor?: string;
+  $style?: CSSProp;
+};
+
+const StyledCircularProgress = styled.div<StyledCircularProgressProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  div {
+    position: absolute;
+    border-radius: 50%;
+    animation: loading-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+
+    ${({ size = 'medium', $circleColor, $style }) => css`
+      width: ${SIZE[size]};
+      height: ${SIZE[size]};
+
+      border: 2px solid ${$circleColor};
+      border-color: ${$circleColor} transparent transparent transparent;
+
+      ${$style}
+    `}
+  }
+
+  div:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+
+  div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+
+  div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+
+  @keyframes loading-ring {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
